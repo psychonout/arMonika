7c11bb48c091fa7647e5769be49a769b2445a7e0
Donatas <donatas@yoursapp.com>
2022-02-19 12:36:43 +0200

    initial commit

---
+++ intercept.js
@@ -0,0 +1,26 @@
+var script = document.createElement("script");
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js";
+script.type = "text/javascript";
+document.getElementsByTagName("head")[0].appendChild(script);
+
+function replaceLinks() {
+  $.each(
+    $(".block-grid-item__component.image.image--link"),
+    function (index, value) {
+      var request = new XMLHttpRequest();
+      request.open(
+        "GET",
+        "https://cdn.jsdelivr.net/gh/psychonout/arMonika@master/" + index
+      );
+      request.onload = function () {
+        if (request.readyState === 4 && request.status === 200) {
+          value.href = request.responseText;
+        }
+      };
+      request.send(null);
+    }
+  );
+}
+
+$(window).on("load", replaceLinks());
+setTimeout(replaceLinks(), 1000);
---
+++ main.py
@@ -0,0 +1,65 @@
+#!/Users/donatasgostautas/.pyenv/shims/python3
+# -*- coding: utf-8 -*-
+import os
+import subprocess
+from datetime import datetime
+
+import paramiko
+import requests
+
+def proxify(url=None):
+    if not url:
+        url = "https://ipinfo.io"
+    username = ""
+    password = ""
+
+    proxy = f"http://{username}:{password}@gate.smartproxy.com:7000"
+
+    response = requests.get(url, proxies={"http": proxy, "https": proxy})
+
+    print(response.text)
+
+
+def find_pages():
+    needles = ["3477", "3478", "3479"]
+    for i in range(1, 10):
+        url = f"https://www.interjeras.lt/balsuok?p={i}"
+        response = requests.get(url)
+        for index, needle in enumerate(needles):
+            if f"vote-{needle}" in response.text:
+                new_url = f"{url}#i{needle}"
+                with open(f"{index}", "rb") as f:
+                    old_url = f.read().strip()
+                if old_url != new_url:
+                    with open(f"{index}", "w") as f:
+                        f.write(new_url.strip())
+        # from bs4 import BeautifulSoup
+        # soup = BeautifulSoup(response.content, 'lxml')
+        # votes = soup.find_all("a", {"class": "vote"})
+        # for vote in votes:
+        #     print(vote.text.split('(')[])
+
+
+def upload_files():
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect("185.224.138.91", username="u829386246", port="65002")
+    sftp = ssh.open_sftp()
+    localpath = "/Users/donatasgostautas/side/arMonika/"
+    remotepath = "/home/u829386246/domains/vystymaskitaip.lt/public_html/"
+    for i in range(3):
+        sftp.put(f"{localpath}{i}", f"{remotepath}{i}")
+    sftp.put(f"{localpath}intercept.js", f"{remotepath}intercept.js")
+    sftp.close()
+    ssh.close()
+
+def push_to_git():
+    subprocess.call(["git", "add", "."])
+    subprocess.call(["git", "commit", "--amend", "--no-edit"])
+    subprocess.call(["git", "push", "-u", "origin", "--force"])
+
+
+if __name__ == "__main__":
+    find_pages()
+    push_to_git()
+    print(f"Success at {datetime.now()}")
